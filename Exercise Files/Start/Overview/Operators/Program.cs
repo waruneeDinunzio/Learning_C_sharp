50c6dd2c4f58d377557484d09ce92a49e9f016a8
warunee dinunzio <dinunziow@gmail.com>
2022-05-09 22:28:58 -0400

    C# oparators

--- Exercise Files/Start/Overview/Operators/Program.cs
+++ Exercise Files/Start/Overview/Operators/Program.cs
@@ -7,19 +7,27 @@ namespace Operators
         static void Main(string[] args)
         {
             // Declare some variables to excercise the operators
-            int x=10, y=5;
-            string a="abcd", b="efgh";
+            int x = 10, y = 5;
+            string a = "abcd", b = "efgh";
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
+
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
 
             // TODO: Operators can be shorthand: a = a + b
-
+            a += b;
+            Console.WriteLine(a);
 
             // TODO: Logical operators &&, ||
             // Console.WriteLine("----- Logic Operators -----");
@@ -29,7 +37,7 @@ namespace Operators
             // string str = null;
             // TODO: the ?? operator uses left operand if not null, or right one if it is
 
-            
+
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
